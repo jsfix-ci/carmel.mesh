e326cfff4c4937cee99ccf425e2409baf740903d
Dan Calinescu <idancali@idancalimac13.station>
2021-09-14 08:15:56 +0200

    added initial structure for ipfs including caching #7

---
+++ src/events/account.ts
@@ -0,0 +1,13 @@
+import { Session } from '..'
+
+export const create_account = async (session: Session, event: any) => {
+}
+
+export const create_account_result = async (session: Session, event: any) => {
+}
+
+export const update_account = async (session: Session, event: any) => {
+}
+
+export const update_account_result = async (session: Session, event: any) => {
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export * from './Identity'
+export * from './Key'
+export * from './Chain'
+export * from './Session'
+export * from './Data'
+export * from './Cache'
+export * from './Node'
+export * from './resolvers'
+export * from './types'
+
+export * as hooks from './hooks'
+export * as events from './events'
